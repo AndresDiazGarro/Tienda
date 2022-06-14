89ba5079bc1b27e34088a27a5e09ce49904624c8
AndresDiazGarro <iandresfy@gmail.com>
2022-06-13 21:19:40 -0600

    Clase #5

--- src/main/java/com/tienda/controller/IndexController.java
+++ src/main/java/com/tienda/controller/IndexController.java
@@ -1,26 +1,25 @@
 package com.tienda.controller;
 
-import com.tienda.dao.ClienteDao;
+import com.tienda.domain.Cliente;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import com.tienda.domain.Cliente;
-import java.util.Arrays;
+import com.tienda.service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @Slf4j
 public class IndexController {
 
     @Autowired
-    private ClienteDao ClienteDao;
-    
+    private ClienteService clienteService;
+
     @GetMapping("/")
     public String inicio(Model model) {
         log.info("Ahora utilizamos MVC");
 
-     /* Cliente cliente = new Cliente("Andrés", "Díaz Garro", "iandrestgn@gmail.com", "8476-7856");
+        /* Cliente cliente = new Cliente("Andrés", "Díaz Garro", "iandrestgn@gmail.com", "8476-7856");
         Cliente cliente2 = new Cliente("Andrés", "Díaz Garro", "iandrestgn@gmail.com", "8476-7856");
         Cliente cliente3 = new Cliente("Andrés", "Díaz Garro", "iandrestgn@gmail.com", "8476-7856");
 
@@ -29,9 +28,34 @@ public class IndexController {
         model.addAttribute("cliente",cliente);
         model.addAttribute("clientes",clientes);
          */
-        var clientes = ClienteDao.findAll();
-        model.addAttribute("clientes",clientes);
-     
-     return "index";
+        var clientes = clienteService.getClientes();
+
+        model.addAttribute("clientes", clientes);
+
+        return "index";
+    }
+
+    @GetMapping("/nuevoCliente")
+    public String nuevoCliente(Cliente cliente) {
+        return "modificarCliente";
+    }
+
+    @PostMapping("/guardarCliente")
+    public String guardarCliente(Cliente cliente) {
+        clienteService.save(cliente);
+        return "redirect:/";
+    }
+
+    @GetMapping("/modificarCliente/{idCliente}")
+    public String modificarCliente(Cliente cliente, Model model) {
+        cliente = clienteService.getCliente(cliente);
+        model.addAttribute("cliente", cliente);
+        return "modificarCliente";
+    }
+
+    @GetMapping("/eliminarCliente/{idCliente}")
+    public String eliminarCliente(Cliente cliente) {
+        clienteService.delete(cliente);
+        return "redirect:/";
     }
 }
---
+++ src/main/java/com/tienda/service/ClienteService.java
@@ -0,0 +1,16 @@
+package com.tienda.service;
+
+import com.tienda.domain.Cliente;
+import java.util.List;
+
+public interface ClienteService {
+
+    public List<Cliente> getClientes();
+
+    public Cliente getCliente(Cliente cliente);
+
+    public void save(Cliente cliente);//Reakiza el trabajo de Create and Edit
+    //Por medio del Id del objeto para saber si es necesario actualizarlo o crearlo del todo
+
+    public void delete(Cliente cliente);
+}
